7cb568aacb9c4d9b1c3d50df5c28a2e9f1498f3d
Alexandre Plennevaux <aplennevaux@MacBook-Pro-Famille.local>
2016-02-06 21:09:56 +0100

    Screenshots!

---
+++ grunt/pageres.js
@@ -0,0 +1,15 @@
+module.exports = {
+	screenshot: {
+		options: {
+			urls: '<%= project.mamp_install %>',
+			css: '',
+			sizes: ['880x660'],
+			dest: '<%= project.src_dir %>',
+			filename: 'screenshot',
+			format: 'png',
+			width: 880,
+			height: 660,
+			crop: true
+		}
+	}
+};
